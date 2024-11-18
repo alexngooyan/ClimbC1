92e7ad5f14a6cd601ec47ab2f9251f43e965650f
Alex Nguyen <alexandn@andrew.cmu.edu>
2024-11-18 01:52:01 -0500

    Appdata for threshold settings is saved across pages, can be accessed for display by home page (right now it takes the min of each threshold for each joint and displays it)

--- app/src/main/java/com/example/climbc1/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/climbc1/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.climbc1.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.climbc1.databinding.FragmentHomeBinding
+import kotlin.math.max
+import kotlin.math.min
 
 class HomeFragment : Fragment() {
 
@@ -28,9 +31,58 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        /*
+        val sharedPref = requireContext().getSharedPreferences("ThresholdPreferences", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
 
-        former code to change text on home to "fuck rectangles"
+        val a2Threshold1 = sharedPref.getString("a2Threshold1", "--")
+        val a2Threshold2 = sharedPref.getString("a2Threshold2", "--")
+        val a4Threshold1 = sharedPref.getString("a4Threshold1", "--")
+        val a4Threshold2 = sharedPref.getString("a4Threshold2", "--")
+
+        //cheese way of getting around kotlin null exception rules
+        var a2Threshold1ToDisplay = 0
+
+        if (!a2Threshold1.isNullOrEmpty()) {
+            val a2Threshold1Value = a2Threshold1.toIntOrNull()
+            if (a2Threshold1Value != null && a2Threshold1Value >= 0) {
+                a2Threshold1ToDisplay = a2Threshold1.toInt()
+            }
+        }
+
+        var a2Threshold2ToDisplay = 0
+
+        if (!a2Threshold2.isNullOrEmpty()) {
+            val a2Threshold2Value = a2Threshold2.toIntOrNull()
+            if (a2Threshold2Value != null && a2Threshold2Value >= 0) {
+                a2Threshold2ToDisplay = a2Threshold2.toInt()
+            }
+        }
+
+        var a4Threshold1ToDisplay = 0
+
+        if (!a4Threshold1.isNullOrEmpty()) {
+            val a4Threshold1Value = a4Threshold1.toIntOrNull()
+            if (a4Threshold1Value != null && a4Threshold1Value >= 0) {
+                a4Threshold1ToDisplay = a4Threshold1.toInt()
+            }
+        }
+
+        var a4Threshold2ToDisplay = 0
+
+        if (!a4Threshold2.isNullOrEmpty()) {
+            val a4Threshold2Value = a4Threshold2.toIntOrNull()
+            if (a4Threshold2Value != null && a4Threshold2Value >= 0) {
+                a4Threshold2ToDisplay = a4Threshold2.toInt()
+            }
+        }
+
+        val a2ThresholdDisplayed = min(a2Threshold1ToDisplay, a2Threshold2ToDisplay)
+        val a4ThresholdDisplayed = min(a4Threshold1ToDisplay, a4Threshold2ToDisplay)
+
+        binding.A2ThresholdText.text = a2ThresholdDisplayed.toString()
+        binding.A4ThresholdText.text = a4ThresholdDisplayed.toString()
+
+        /*former code to change text on home to "fuck rectangles"
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
